3abac589ba1b41817c294aeb3ecbcdc58e10071e
Nico <nicogaldamez@gmail.com>
2013-04-24 20:51:35 -0300

    Versión que anda a Nico

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sch0larisRepo::Application.config.secret_token = 'b7d185aad9aa9986213ace227bfbff55a4ec1d77e53ba199a7f36645d1416cbaeb7269ead3175ce3c74d30a182e0a5e4d3b6c8333929a8ea7b31f517caebee35'
