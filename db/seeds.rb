5fa270f6c8bcb6f40ecf7ee05393ee7323fa2c34
Nico <nicogaldamez@gmail.com>
2013-11-27 11:10:53 -0300

    Agregué Universitas y UBA

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(name: "Don Scholaris", email: 'test@scholaris.com', password: 'Foobar123', 
-      password_confirmation: "Foobar123")
+# encoding: UTF-8
+
+# UNIVERSITAS
+School.find_or_create_by_name("Universitas - Instrumentación Qui")
+School.find_or_create_by_name("Universitas - Radiología")
+School.find_or_create_by_name("Universitas - Turismo")
+School.find_or_create_by_name("Universitas - Recursos Humanos")
+School.find_or_create_by_name("Universitas - Hotelería")
+School.find_or_create_by_name("Universitas - Analista de Sistemas")
+School.find_or_create_by_name("Universitas - Administración de Empresas")
+School.find_or_create_by_name("Universitas - Marketing")
+School.find_or_create_by_name("Universitas - Relaciones Públicas")
+School.find_or_create_by_name("Universitas - Organización de Eventos")
+School.find_or_create_by_name("Universitas - Diseño de Indumentaria")
+
+# UBA
+School.find_or_create_by_name("UBA - Agronomía")
+School.find_or_create_by_name("UBA - Arquitectura, Diseño y Urbanismo")
+School.find_or_create_by_name("UBA - Ciencias Económicas")
+School.find_or_create_by_name("UBA - Ciencias Exactas y Naturales")
+School.find_or_create_by_name("UBA - Ciencias Sociales")
+School.find_or_create_by_name("UBA - Ciencias Veterinarias")
+School.find_or_create_by_name("UBA - Derecho")
+School.find_or_create_by_name("UBA - Farmacia y Bioquímica")
+School.find_or_create_by_name("UBA - Filosofía y Letras")
+School.find_or_create_by_name("UBA - Ingeniería")
+School.find_or_create_by_name("UBA - Medicina")
+School.find_or_create_by_name("UBA - Odontología")
+School.find_or_create_by_name("UBA - Psicología")
